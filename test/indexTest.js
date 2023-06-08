bbb4d66980db823c0bdb8a9ee0dd8672c990750f
lydia tschoe <lydiatschoe@gmail.com>
2023-06-08 13:16:20 -0400

    .results.json

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,12 +7,46 @@ describe('index.js', function () {
     });
   });
 
+  const cats = ["Milo", "Otis", "Garfield"];
+
   describe('Array functions', function () {
     beforeEach(function () {
       cats.length = 0;
-
       cats.push('Milo', 'Otis', 'Garfield');
     });
+
+  
+    function destructivelyAppendCat(name) {
+      cats.push(name);
+    }
+
+    function destructivelyPrependCat(name) {
+      cats.unshift(name);
+    }
+
+    function destructivelyRemoveLastCat() {
+      cats.pop();
+    }
+
+    function destructivelyRemoveFirstCat() {
+      cats.shift();
+    }
+
+    function appendCat(name) {
+      return [...cats, name];
+    }
+
+    function prependCat(name) {
+      return [name, ...cats];
+    }
+
+    function removeLastCat() {
+      return cats.slice(0, -1);
+    }
+
+    function removeFirstCat() {
+      return cats.slice(1);
+    }
 
     describe('destructivelyAppendCat(name)', function () {
       it('appends a cat to the end of the cats array', function () {
